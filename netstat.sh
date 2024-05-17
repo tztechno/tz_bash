e0849bd7ac6960ba2ba332cec51f825d1f1850bf
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-05-18 08:48:27 +0900

    Create netstat.sh

---
+++ netstat.sh
@@ -0,0 +1,17 @@
+
+[使ってるポートを調べる]
+
+% sudo netstat -ntlp | grep LISTEN
+
+Password:
+
+netstat: option requires an argument -- p
+Usage:	netstat [-AaLlnW] [-f address_family | -p protocol]
+	netstat [-gilns] [-f address_family]
+	netstat -i | -I interface [-w wait] [-abdgRtS]
+	netstat -s [-s] [-f address_family | -p protocol] [-w wait]
+	netstat -i | -I interface -s [-f address_family | -p protocol]
+	netstat -m [-m]
+	netstat -r [-Aaln] [-f address_family]
+	netstat -rs [-s]
+
